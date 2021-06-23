34193ef6a6d7eba3bc4091ef573b484c63eab802
meetpahul <46177801+meetpahul@users.noreply.github.com>
2021-06-22 20:02:15 -0400

    Updated function documentation

--- generalFunctions.js
+++ generalFunctions.js
@@ -7,6 +7,7 @@
   Result - This function returns a single-dimensional array.
 
 */
+
 function flattenArray(array){
   var flatArray = []
   for (var i = 0; i < array.length; i++) {
@@ -15,6 +16,17 @@ function flattenArray(array){
   return flatArray
 }
 
+/* 
+  If you have two arrays, 
+  and you want to get the indices of elements of array 1 in the array 2, 
+  Then this function will get the indices in a new array for you.
+  
+  Parameter - This function takes the two one-dimensional arrays as parameters. 
+  Result - This function returns one single-dimensional array which contains indices of elements of array1 in array 2.
+  
+  Note: If an element in array1 is not included in array2, this function does not throw an error. 
+  This function returns indices of elements that exist in both arrays.
+*/
 
 // General Function
 function index(array1, array2){
